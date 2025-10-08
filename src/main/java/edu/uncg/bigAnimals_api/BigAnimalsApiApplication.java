366673cf8294b3724db8b691c4041ac065607065
WTedwards-Work <WTEDWARDS@uncg.edu>
2025-10-08 11:50:06 -0400

    Added Spring Boot scaffolding

---
+++ src/main/java/edu/uncg/bigAnimals_api/BigAnimalsApiApplication.java
@@ -0,0 +1,13 @@
+package edu.uncg.bigAnimals_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigAnimalsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigAnimalsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uncg/bigAnimals_api/BigAnimalsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uncg.bigAnimals_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigAnimalsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
